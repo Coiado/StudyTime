d060578cea4891617d2c32a95394fded38aee3a2
Ogari Pata Pacheco <OgariPPacheco@MacBook-Pro-de-Ogari.local>
2015-07-19 00:40:22 -0300

    (00:39) Ogari: Mudado um código muito louco, para a integração de dados com os gráficos

--- JodApp/MateriaTeste1ViewController.swift
+++ JodApp/MateriaTeste1ViewController.swift
@@ -10,6 +10,7 @@ import Charts
 import Parse
 
 
+
 class MateriaTeste1ViewController: UIViewController {
     
  //   public class BarLineChartViewBase: ChartViewBase, UIGestureRecognizerDelegate <- Ver!
@@ -29,11 +30,11 @@ class MateriaTeste1ViewController: UIViewController {
    // var yAxis : ChartYAxis!
         
     // Onde estão todas as semanas de estudo do aluno
-    let currentTimeStudied = [0.5, 3.0, 0.5, 0.0, 1.0, 1.0, 1.5] // 7.5h totais
+    var currentTimeStudied = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] // 7.5h totais
     var pastTimeStudied1 = [1.0, 0.5, 0.5, 1.0, 1.5, 0.0, 0.0] // 4.5h totais
     var pastTimeStudied2 = [0.5, 1.5, 1.0, 0.5, 0.5, 0.0, 0.5] // 4.5h totais
     var pastTimeStudied3 = [4.5, 1.0, 1.5, 1.0, 1.5, 0.5, 0.5] // 7.5h totais
-    let days = [NSLocalizedString("Mon", comment: "Monday"), NSLocalizedString("Tue", comment: "Tuesday"), NSLocalizedString("Wed", comment: "Wednesday"), NSLocalizedString("Thu", comment: "Thursday"), NSLocalizedString("Fri", comment: "Friday"), NSLocalizedString("Sat", comment: "Saturday"), NSLocalizedString("Sun", comment: "Sunday")]
+    var currentDays = [NSLocalizedString("Mon", comment: "Monday"), NSLocalizedString("Tue", comment: "Tuesday"), NSLocalizedString("Wed", comment: "Wednesday"), NSLocalizedString("Thu", comment: "Thursday"), NSLocalizedString("Fri", comment: "Friday"), NSLocalizedString("Sat", comment: "Saturday"), NSLocalizedString("Sun", comment: "Sunday")]
     
     // Contador das "páginas" dos gráficos de semanas passadas
     var cont : Int8 = 0
@@ -57,6 +58,209 @@ class MateriaTeste1ViewController: UIViewController {
         
 //        pastLineChartView.borderLineWidth = 3.0
 //        pastLineChartView.borderColor = UIColor.blueColor()
+        
+    }
+    
+    // Metodo que retorna o dia da semana, dado o valor de entrada de um weekDay
+    func auxDiaDaSemana(dia: Int) -> String{
+    
+        switch dia
+        {
+        case 1:
+            return "Sun"
+        case 2:
+            return "Mon"
+        case 3:
+            return "Tue"
+        case 4:
+            return "Wed"
+        case 5:
+            return "Thu"
+        case 6:
+            return "Fri"
+        case 7:
+            return "Sat"
+        default:
+            println("ERROR! Dia da semana inexistente!")
+            return ""
+        }
+    }
+    
+    func getChartsData(nomeDoAluno: String, materiaEstudada : Int){
+
+        // Define-se a variacao de dias total, desde quando ele comecou até o dia atual
+        var comecou = "2010-09-01" // NSDate()
+        var parou = "2010-09-05"
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        var diaInicial:NSDate = dateFormatter.dateFromString(comecou)!
+        var diaFinal:NSDate = dateFormatter.dateFromString(parou)!
+        
+        let calendario = NSCalendar.currentCalendar()
+        let unit:NSCalendarUnit = .CalendarUnitDay
+        
+        let components = calendario.components(unit, fromDate: diaInicial, toDate: diaFinal, options: nil)
+        
+
+        
+        // Recebida a variação total de dias
+        var  totalDeDias : Int =  components.day
+        var totalDeSemanas : Int
+        
+        var auxDeCurrentDays : Int = 0
+        
+        //Define-se o total de semanas
+            if (totalDeDias%7 == 0) { totalDeSemanas = totalDeDias/7 }
+        
+            else {totalDeSemanas = (totalDeDias/7) + 1}
+        
+//------------------------------  ------------------------------  ------------------------------
+//                                Funções para o gráfico atual
+//------------------------------  ------------------------------  ------------------------------
+        
+        // Caso ele não tenha uma semana completa, segue a função
+            if (totalDeDias%7 != 0)
+            {
+            
+            var aux : Int = ((totalDeDias%7)) // Por enquanto acho q agora foi.
+                
+                // Responsavel por definir primeira semana (dados a serem plotados no gráfico de cima)
+                for var i = 0; i < aux; i++
+                {
+                    var  primeiroDiaDaSemana : NSDate = diaFinal.dateByAddingTimeInterval(Double(-(((aux - 1) + i)*24*60*60)))
+                    
+                    var itensDaData = calendario.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitWeekday, fromDate: primeiroDiaDaSemana)
+                    
+                    // É feita e afunilada a pesquisa para se pegar o dado de tempo, no determinado dia de estudo
+                    var query = PFQuery(className:"StudyTime")
+                    
+                    query.whereKey("aluno", matchesRegex: nomeDoAluno)
+                    query.whereKey( "materia", equalTo: materiaEstudada)
+                    query.whereKey( "dia", equalTo: itensDaData.day)
+                    query.whereKey( "mes", equalTo: itensDaData.month)
+                    query.whereKey( "ano", equalTo: itensDaData.year)
+                    
+                    // Passa os dias dessa semana
+                    currentDays[i] = NSLocalizedString(auxDiaDaSemana(itensDaData.weekday), comment: "dias da semana")
+                    
+                    //Assim, na última rodada do FOR, terá acumulado o último dia dessa semana
+                    auxDeCurrentDays = itensDaData.weekday
+                    
+                    var tempo : Array = query.findObjects()!
+                    var tempoTotal : Double = 0.0
+                    
+                        if tempo.isEmpty { currentTimeStudied[i] = 0.0 } // Verifica se o aluno não estudou nada da matéria no dia
+                    
+                        else
+                        {
+                            // Calcula as horas totais de estudo do cara, no dia. Já que ele pode terminar e voltar a estudar a mesma matéria ao longo do dia
+                            for var j = 0; j < tempo.count; j++
+                            {
+                                tempoTotal = tempoTotal + (tempo[j]["tempo"] as! Double)
+                            }
+                            currentTimeStudied[i] = tempoTotal
+                        }
+                    
+              //      itensDaData.weekday -> 1 is Sunday, 2 is Monday and so forth...
+                    
+                }
+                
+                // Essa parte serve para preencher a semana com os demais até completar 7, por mais que eles não tenham ocorrido ainda
+                for var m = 0; m < (7 - currentDays.count); m++
+                {
+                
+                    if (auxDeCurrentDays + 1) < 8
+                    { currentDays.append(NSLocalizedString(auxDiaDaSemana(auxDeCurrentDays), comment: "dias da semana"))
+                        auxDeCurrentDays = auxDeCurrentDays + 1
+                    }
+                    
+                    else {
+                        auxDeCurrentDays = auxDeCurrentDays - 7
+                        currentDays.append(NSLocalizedString(auxDiaDaSemana(auxDeCurrentDays), comment: "dias da semana"))
+                        }
+                    
+                }
+                
+                // Abaixo, completamos o gráfico com 0 horas, nos dias que faltam (ainda nem chegaram)
+                for var n = 0; n < 7 - currentTimeStudied.count; n++ {currentTimeStudied.append(0.0)}
+            }
+        
+            // Funcao para o caso de uma semana normal, de 7 dias (extremamente semelhante á funcao de cima)
+            else
+            {
+                for var i = -6; i < 1; i++
+                {
+                    var  primeiroDiaDaSemana : NSDate = diaFinal.dateByAddingTimeInterval(Double(-((i)*24*60*60)))
+                    
+                    var itensDaData = calendario.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitWeekday, fromDate: primeiroDiaDaSemana)
+                    
+                    var query = PFQuery(className:"StudyTime")
+                    
+                    query.whereKey("aluno", matchesRegex: nomeDoAluno)
+                    query.whereKey( "materia", equalTo: materiaEstudada)
+                    query.whereKey( "dia", equalTo: itensDaData.day)
+                    query.whereKey( "mes", equalTo: itensDaData.month)
+                    query.whereKey( "ano", equalTo: itensDaData.year)
+                    
+                    currentDays[i + 6] = NSLocalizedString(auxDiaDaSemana(itensDaData.weekday), comment: "dias da semana")
+                    
+                    auxDeCurrentDays = itensDaData.weekday
+                    
+                    var tempo : Array = query.findObjects()!
+                    var tempoTotal : Double = 0.0
+                    
+                    if tempo.isEmpty { currentTimeStudied[i + 6] = 0.0 } // Verifica se o aluno não estudou nada da matéria no dia
+                        
+                    else
+                    {
+                        for var j = 0; j < tempo.count; j++
+                        {
+                            tempoTotal = tempoTotal + (tempo[j]["tempo"] as! Double)
+                        }
+                        currentTimeStudied[i + 6] = tempoTotal
+                    }
+                    
+                    //      itensDaData.weekday -> 1 is Sunday, 2 is Monday and so forth...
+                    
+                }
+                
+                // Essa parte serve para preencher a semana com os demais até completar 7, por mais que eles não tenham ocorrido ainda
+                for var m = 0; m < (7 - currentDays.count); m++
+                {
+                    
+                    if (auxDeCurrentDays + 1) < 8
+                    { currentDays.append(NSLocalizedString(auxDiaDaSemana(auxDeCurrentDays), comment: "dias da semana"))
+                        auxDeCurrentDays = auxDeCurrentDays + 1
+                    }
+                        
+                    else {
+                        auxDeCurrentDays = auxDeCurrentDays - 7
+                        currentDays.append(NSLocalizedString(auxDiaDaSemana(auxDeCurrentDays), comment: "dias da semana"))
+                    }
+                    
+                }
+
+            }
+        
+        //------------------------------  ------------------------------  ------------------------------
+        //                                Funções para os gráficos passados
+        //------------------------------  ------------------------------  ------------------------------
+        
+        var dadosDeGraficosPassados : Array<Array<Double>>
+        var semanaEmQuestao : Array<Double>
+        
+        for var a = 0; a < (totalDeSemanas - 1); a++
+        {
+        
+            for var b = 0; b < 7; b++ {
+            
+//                semanaEmQuestao[b] = diaInicial
+                
+            }
+            
+        }
         
     }
     
@@ -170,7 +374,7 @@ class MateriaTeste1ViewController: UIViewController {
 //                let yAxisMaxInitial = currentLineChartView.leftAxis.customAxisMax
                 currentLineChartView.leftAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied2)
                 currentLineChartView.rightAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied2)
-                setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied2)
+                setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied2)
                 
 //                // Animacao de gráfico de baixo para cima.
 //                currentLineChartView.animate(yAxisDuration: 2.0, easing: { (elapsed, duration) -> CGFloat in
@@ -195,7 +399,7 @@ class MateriaTeste1ViewController: UIViewController {
             
             }
             
-            setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied2)
+            setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied2)
             pastLineChartView.animate(yAxisDuration: 2.0)
             
         case 2:
@@ -219,7 +423,7 @@ class MateriaTeste1ViewController: UIViewController {
             {
                 currentLineChartView.leftAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied3)
                 currentLineChartView.rightAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied3)
-                setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied3)
+                setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied3)
 //                currentLineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: ChartEasingOption.Linear)
                 
                 UIView.animateWithDuration(1.3, delay: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
@@ -238,7 +442,7 @@ class MateriaTeste1ViewController: UIViewController {
                 
             }
             
-            setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied3)
+            setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied3)
             pastLineChartView.animate(yAxisDuration: 2.0)
             
         default:
@@ -259,7 +463,7 @@ class MateriaTeste1ViewController: UIViewController {
             {
                 currentLineChartView.leftAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied1)
                 currentLineChartView.rightAxis.customAxisMax = highestHourComparison(currentTimeStudied, week2: pastTimeStudied1)
-                setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied1)
+                setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied1)
 //                currentLineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: ChartEasingOption.Linear)
                 
                 UIView.animateWithDuration(1.3, delay: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
@@ -279,7 +483,7 @@ class MateriaTeste1ViewController: UIViewController {
                 
             }
             
-            setChart(days, currentValues: currentTimeStudied, pastValues: pastTimeStudied1)
+            setChart(currentDays, currentValues: currentTimeStudied, pastValues: pastTimeStudied1)
             pastLineChartView.animate(yAxisDuration: 2.0)
             //            println("Test")
             
